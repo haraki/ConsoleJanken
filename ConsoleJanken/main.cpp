1512ac899f53afe3821a5a289bd9d9a97aa3eb71
Masashi Haraki <masa.haraki@gmail.com>
2014-04-21 22:27:00 +0900

    Initial Commit

---
+++ ConsoleJanken/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ConsoleJanken
+//
+//  Created by 原木 正志 on 2014/04/21.
+//  Copyright (c) 2014年 Masashi Haraki. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
